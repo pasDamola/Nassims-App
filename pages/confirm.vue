332ad7ecb08283e3c6d8d8c282fe142561ec7a7c
Oyinda David <oyindadavid18@gmail.com>
2020-09-11 15:06:17 +0100

    Application working

---
+++ pages/confirm.vue
@@ -0,0 +1,50 @@
+<template>
+  <main class="main3">
+    <div class="app_submit">
+      <img src="/Group 3.svg" alt="logo">
+      <h3 class="hooray-application">
+        Hooray! Application Submitted
+      </h3>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <p class="nbatch-paragraph">
+        Your Application for N-Power Batch C has been Submitted succesfully
+      </p>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+    </div>
+  </main>
+</template>
+
+<style>
+.main3 {
+  padding: 100px;
+  margin-bottom: 20px;
+}
+
+.app_submit {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+}
+
+.hooray-application {
+  color: #25c470;
+  font-family: Poppins;
+  font-size: 24px;
+  font-weight: 500;
+  letter-spacing: 0;
+}
+
+.nbatch-paragraph {
+  color: #a5a5a5;
+  font-family: Poppins;
+  font-size: 16px;
+  font-weight: 500;
+  letter-spacing: 0;
+  text-align: center;
+  margin-top: -40px;
+}
+</style>
